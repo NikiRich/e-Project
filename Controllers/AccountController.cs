28a918afe0dbd17998aa88837f1e887e242f1e10
NikiRich <bogatyveron3@gmail.com>
2024-03-03 00:48:46 +0200

    v. 0.0.3
    -Added Account Controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using e_Project.Models;
+
+namespace e_Project.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (_context.Users.Any(u => u.Email == user.Email))
+            {
+                ModelState.AddModelError("Email", "Email already exists.");
+                return View(user);
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(user);
+        }
+    }
+}
