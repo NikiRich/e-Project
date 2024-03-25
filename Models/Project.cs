22ff9964fa8750a69395d777a34da9ab95570ac0
NikiRich <bogatyveron3@gmail.com>
2024-03-25 22:51:41 +0200

    v. 0.2.2
    -Added the Project model

---
+++ Models/Project.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace e_Project.Models
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required(ErrorMessage = "Author is required.")]
+        [StringLength(30, ErrorMessage = "Author name cannot exceed 30 characters.")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(40, ErrorMessage = "Title cannot exceed 40 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Programming Language is required.")]
+        [StringLength(15, ErrorMessage = "Programming Language cannot exceed 10 characters.")]
+        public string ProgrammingLanguage { get; set; }
+
+        [Required(ErrorMessage = "Status is required.")]
+        public ProjectStatus Status { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters.")]
+        public string Description { get; set; }
+    }
+
+    public enum ProjectStatus
+    {
+        Pending,
+        InProgress,
+        Completed
+    }
+}
