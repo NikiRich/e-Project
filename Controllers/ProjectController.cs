4b3785e7180f18cf14f99f1737ccec99010ee688
NikiRich <bogatyveron3@gmail.com>
2024-03-25 22:50:20 +0200

    v. 0.1.9
    -Added a controller for Project

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using e_Project.Models;
+
+namespace e_Project.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+       private readonly ApplicationDbContext _context;
+
+        public ProjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet] 
+        public IActionResult AddProject()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddProject(Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Projects.Add(project);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index","Home");
+            }
+            return View(project);
+        }
+    }
+}
