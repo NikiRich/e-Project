55183297547ea8ea02bdec8512c25b64f6514696
NikiRich <bogatyveron3@gmail.com>
2024-03-28 22:58:38 +0200

    v. 0.2.4
    - Modified Home Controller to list only 5 projects

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using e_Project.Models;
 
 namespace e_Project.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var recentProjects = _context.Projects
+                 .Where(p => p.Status == ProjectStatus.Completed)
+                 .OrderByDescending(p => p.CompletionTime)
+                 .Take(5)
+                 .ToList();
+
+            return View(recentProjects);
         }
     }
 }
