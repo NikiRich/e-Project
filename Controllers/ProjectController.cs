e63d80f3acce5dcdc89d94661b0c894741c57fd1
NikiRich <bogatyveron3@gmail.com>
2024-04-04 16:58:48 +0300

    v. 0.2.8
    -Modified teh Project Controller

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using e_Project.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace e_Project.Controllers
 {
     [Authorize]
     public class ProjectController : Controller
     {
-       private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public ProjectController(ApplicationDbContext context)
         {
             _context = context;
         }
-        [HttpGet] 
+        [HttpGet]
         public IActionResult AddProject()
         {
-            return View(); 
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
         }
 
         [HttpPost]
@@ -27,9 +34,69 @@ namespace e_Project.Controllers
             {
                 _context.Projects.Add(project);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
             return View(project);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string query)
+        {
+            List<Project> projects = new List<Project>();
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                var matches = await _context.Projects.Where(p => p.Title.Contains(query)
+                || p.Description.Contains(query) || p.Author.Contains(query)
+                || p.ProgrammingLanguage.Contains(query))
+                    .ToListAsync();
+
+                projects = matches.Select(p => new
+                {
+                    Project = p,
+                    Occurences = (p.Title.CountOccurrences(query) +
+                    p.Description.CountOccurrences(query) + p.Author.CountOccurrences(query) +
+                    p.ProgrammingLanguage.CountOccurrences(query))
+                }).OrderByDescending(p => p.Occurences).Select(p => p.Project).ToList();
+            }
+
+            ViewBag.query = query;
+            return View(projects);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project != null && (project.Status == ProjectStatus.Pending || project.Status == ProjectStatus.Completed))
+            {
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Home");
+        }
     }
+    public static class Extension
+    {
+        public static int CountOccurrences(this string target, string value)
+        {
+            if (string.IsNullOrEmpty(target) || string.IsNullOrEmpty(value))
+            {
+                return 0;
+            }
+
+            int count = 0;
+            int i = 0;
+            while ((i = target.IndexOf(value, i, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                i += value.Length;
+                count++;
+            }
+
+            return count;
+        }
+    }
+
 }
