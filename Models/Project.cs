2bed7c66dc6dedfb9cbac00f6844c75bbd1ce5a3
NikiRich <bogatyveron3@gmail.com>
2024-04-11 22:25:33 +0300

    v. 0.3.4
    -Modified Project Model

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace e_Project.Models
 {
@@ -9,25 +10,32 @@ namespace e_Project.Models
 
         [Required(ErrorMessage = "Author is required.")]
         [StringLength(30, ErrorMessage = "Author name cannot exceed 30 characters.")]
+        [Display(Name ="Author")]
         public string Author { get; set; }
 
         [Required(ErrorMessage = "Title is required.")]
         [StringLength(40, ErrorMessage = "Title cannot exceed 40 characters.")]
+        [Display(Name = "Title")]
         public string Title { get; set; }
 
         [Required(ErrorMessage = "Programming Language is required.")]
-        [StringLength(15, ErrorMessage = "Programming Language cannot exceed 10 characters.")]
+        [StringLength(15, ErrorMessage = "Programming Language cannot exceed 15 characters.")]
+        [Display(Name ="Programming Language")]
         public string ProgrammingLanguage { get; set; }
 
         [Required(ErrorMessage = "Status is required.")]
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public ProjectStatus Status { get; set; }
 
         [Required(ErrorMessage = "Description is required.")]
         [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters.")]
+        [Display(Name ="Description")]
         public string Description { get; set; }
 
         [Required(ErrorMessage = "Completion Time is required.")]
+        [Display(Name ="Completion Time")]
         public int CompletionTime { get; set; }
+
     }
 
     public enum ProjectStatus
@@ -36,4 +44,5 @@ namespace e_Project.Models
         InProgress,
         Completed
     }
+   
 }
